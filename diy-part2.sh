145ef11351a48ed33f5574d43f59a4d17695bb1a
SZ-MB <49933875+SZ-MB@users.noreply.github.com>
2022-02-16 05:33:14 +0000

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,10 +10,10 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-sed -i 's/zh_cn/en/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/zh_cn/auto/g' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/Shanghai/Taipei/g' package/lean/default-settings/files/zzz-default-settings
-sed -i 's/zh_cn/en/g' feeds/luci/modules/luci-base/root/etc/uci-defaults/luci-base
-sed -i 's/192.168.1.1/192.168.6.5/g' package/base-files/files/bin/config_generate
+sed -i 's/zh_cn/auto/g' feeds/luci/modules/luci-base/root/etc/uci-defaults/luci-base
+sed -i 's/192.168.1.1/192.168.6.3/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/SZ-MB/g' package/base-files/files/bin/config_generate
 sed -i 's/ntp.aliyun.com/time.windows.com/g' package/base-files/files/bin/config_generate
 sed -i 's/time1.cloud.tencent.com/time.cloudflare.com/g' package/base-files/files/bin/config_generate
